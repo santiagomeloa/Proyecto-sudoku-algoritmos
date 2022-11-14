69a8a6eb299bda7a63439504ea52ff27f6ae9cd2
4Santi8 <santiagomelo48@gmail.com>
2022-11-14 18:13:52 -0500

    santiago -endsprint

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import pygame, sys
-from pygame.locals import *
-import functions, sprites
+import sys
 
-from functions import WIDTH, HEIGHT
+import pygame
+from pygame.locals import *
+
+import functions
+from functions import HEIGHT, WHITE, WIDTH
+from sprites import Casilla, Cuadricula, Numbers, Subcuadricula
 
 FPS = 60
-WHITE = (255, 255, 255)
+
 
 
 def main():
@@ -13,11 +16,36 @@ def main():
     clock = pygame.time.Clock()
     pygame.display.set_caption('Sudoku')
     background_image = functions.load_image("Imagenes/Fondo.png", WIDTH/2, HEIGHT/2, True)
-    
-    cuadricula = sprites.Cuadricula(3, 3, (WIDTH/2, HEIGHT/2))
 
-    # c = sprites.Casilla(0, (WIDTH/2, None, None, None, None, None))
-    # sub = sprites.Subcuadricula(3,3,(None, None, None, None, WIDTH/2, HEIGHT/2))
+    #-------------OBJETOS------------
+
+    numbers_group = pygame.sprite.Group()
+    
+    cuadricula = Cuadricula(3, 3, (WIDTH/2, HEIGHT/2))
+
+    #Objetos Numero
+    one = Numbers(pygame.image.load('Imagenes/bt1.png').convert_alpha(), 0, 0, "1")
+    two = Numbers(pygame.image.load('Imagenes/bt2.png').convert_alpha(), 150, 0, "2")
+    three = Numbers(pygame.image.load('Imagenes/bt3.png').convert_alpha(), 300, 0, "3")
+    four = Numbers(pygame.image.load('Imagenes/bt4.png').convert_alpha(), 0, 150, "4")
+    five = Numbers(pygame.image.load('Imagenes/bt5.png').convert_alpha(), 150, 150, "5")
+    six = Numbers(pygame.image.load('Imagenes/bt6.png').convert_alpha(), 300, 150, "6")
+    seven = Numbers(pygame.image.load('Imagenes/bt7.png').convert_alpha(), 0, 300, "7")
+    eight = Numbers(pygame.image.load('Imagenes/bt8.png').convert_alpha(), 150, 300, "8")
+    nine = Numbers(pygame.image.load('Imagenes/bt9.png').convert_alpha(), 300, 300, "9")
+
+    numbers_group.add(one)
+    numbers_group.add(two)
+    numbers_group.add(three)
+    numbers_group.add(four)
+    numbers_group.add(five)
+    numbers_group.add(six)
+    numbers_group.add(seven)
+    numbers_group.add(eight)
+    numbers_group.add(nine)
+
+    # c = Casilla(0, (WIDTH/2, None, None, None, None, None))
+    # sub = Subcuadricula(3,3,(None, None, None, None, WIDTH/2, HEIGHT/2))
 
 
     #---------------------------------------------------------------------------------------------------------
@@ -30,6 +58,7 @@ def main():
         screen.blit(background_image, (0, 0))
 
         cuadricula.draw(screen)
+        numbers_group.draw(screen)
         # sub.draw(screen)
         # c.draw(screen)
         #-------------detecting keyboards inputs-------------
