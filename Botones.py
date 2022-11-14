827160515c22b8b947e389b27fe189f78f7c2bfd
davinchooo <112956569+davinchooo@users.noreply.github.com>
2022-11-14 15:50:52 -0500

    Update Botones.py

--- Botones.py
+++ Botones.py
@@ -2,18 +2,22 @@ import pygame, sys
 from pygame.locals import *
 
 class Botones:
-    def __init__(self, vl):
+    # Constructor
+    def __init__(self, vl = 0):
         self.valor = vl
         self.SCREEN_WIDTH = 640
         self.SCREEN_HEIGHT = 480
         self.color = (255,255,255)
-        
+    
+    # Getters
     def getVal(self):
         return self.valor
     
+    # Setters
     def setVal(self, vl):
         self.valor = vl
-        
+    
+    # Metodo para observar los botones segun el numero que tengan
     def visualizar(self):
         pygame.init()
     
@@ -21,9 +25,10 @@ class Botones:
         screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
         pygame.display.set_caption("BOTONES")
         
+        # Asigna la imagen segun el numero que tenga establecido
         if self.valor == 1:
-            v = pygame.image.load("Imagenes/bt1.png").convert_alpha()
-            v_c = pygame.transform.scale(v, (200, 100))
+            v = pygame.image.load("Imagenes/bt1.png").convert_alpha() # Se carga la imagen del boton
+            v_c = pygame.transform.scale(v, (200, 100)) # Se modifica el tamaño de la imagen
         if self.valor == 2:
             v = pygame.image.load("Imagenes/bt2.png").convert_alpha()
             v_c = pygame.transform.scale(v, (200, 100))
@@ -50,9 +55,10 @@ class Botones:
             v_c = pygame.transform.scale(v, (200, 100))
         else:
             pass            
-        # Indicamos la posicion de las "Surface" sobre la ventana
+        # Se le asigna un color de fondo y se le indica la posicion de las imagenes sobre la ventana
         screen.fill(self.color)
         screen.blit(v_c, (0, 0))
+        
         # se muestran lo cambios en pantalla
         pygame.display.flip()
     
